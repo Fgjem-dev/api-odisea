bab2903c37300600697c90087ca53c33510149b3
JoelGutierrezN <al221910321@gmail.com>
2025-06-30 14:22:09 -0600

    Update database config: disable encryption and remove redundant options.

--- config/database.php
+++ config/database.php
@@ -108,14 +108,12 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+            'encrypt' => false,
             'trust_server_certificate' => true,
-            'encrypt' => 'Optional',
             'options' => [
                 PDO::SQLSRV_ATTR_ENCODING => PDO::SQLSRV_ENCODING_UTF8,
-                'TrustServerCertificate' => 'on',
             ],
         ]
-
     ],
 
     /*
