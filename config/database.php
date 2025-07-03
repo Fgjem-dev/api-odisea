0f81e9308e925f1cc90fe27d6da15cbf9e8b8c5f
Guillermo Pichardo Nieto <tats59@hotmail.com>
2025-07-03 16:10:53 -0600

    modificacion archivo databse.php #7

--- config/database.php
+++ config/database.php
@@ -108,8 +108,8 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', true),
-            // 'encrypt' => env('DB_ENCRYPT', false),
+            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', true),
+            'encrypt' => env('DB_ENCRYPT', false),
             'app_name' => 'Laravel Application',
         ],
 
